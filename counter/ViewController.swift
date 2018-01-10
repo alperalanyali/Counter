040d5101a32cd0269abb5d0288d068c9044e5ed3
Alper alanyalı <alperalanyali@alper-macbook-pro.local>
2018-01-10 20:13:04 +0300

    Creating Counter

--- counter/ViewController.swift
+++ counter/ViewController.swift
@@ -9,7 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var seconds = 30
+    var timer = Timer()
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var sliderOutlet: UISlider!
+    @IBAction func slider(_ sender: UISlider) {
+        
+        seconds = Int(sender.value)
+        label.text = String(seconds)
+        
+        
+    }
+    
+    @IBOutlet weak var startOutlet: UIButton!
+    @IBAction func start(_ sender: UIButton) {
+        
+         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.counter), userInfo: nil, repeats: true)
+        
+    }
+    @objc func counter(){
+        seconds = seconds - 1
+        label.text = String(seconds)
+        
+        if (seconds == 0){
+            
+            timer.invalidate()
+        }
+        
+        
+    }
+    @IBOutlet weak var stopOutlet: UIButton!
+    @IBAction func stop(_ sender: UIButton) {
+        
+        timer.invalidate()
+        seconds = 30
+        sliderOutlet.setValue(30, animated: true)
+        label.text = "30 Seconds"
+        
+        
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
